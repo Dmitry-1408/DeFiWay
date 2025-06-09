a55e692181047756459b22115b95cf37ff9d2d0e
Dmitry Bykov <bykovdmnic@outlook.com>
2025-06-09 16:55:49 +0300

    завершил spoiler

--- src/js/spoiler.js
+++ src/js/spoiler.js
@@ -1,3 +1,18 @@
 "use strict";
 
+const wrappers = document.querySelectorAll(".accordion__wrapper");
 
+wrappers.forEach((wrapper) => {
+  const button = wrapper.querySelector(".accordion__cross");
+
+  button.addEventListener("click", () => {
+    // Если нужно, чтобы только один блок открывался:
+    wrappers.forEach((i) => {
+      if (i !== wrapper) {
+        i.classList.remove("active");
+      }
+    });
+
+    wrapper.classList.toggle("active");
+  });
+});
